69fffbf3814fbf58e1a2f362f035364622653b66
jluo117 <luojames52@gmail.com>
2017-11-11 10:21:44 -0800

    ready to go

---
+++ King of Space/src/thanos.swift
@@ -0,0 +1,13 @@
+//
+//  thanos.swift
+//  King of Space
+//
+//  Created by james luo on 11/11/17.
+//  Copyright © 2017 james luo. All rights reserved.
+//
+
+import Foundation
+class thanos: ship {
+    ship.hp = 100
+    
+}
